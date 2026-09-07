8b5cd407113055b8658ae5313d7f90f4d6123cd2
elixcat <142026724+elixcat@users.noreply.github.com>
2026-09-07 08:16:30 +0300

    Update and rename subtitles

--- subtitles-ukr.js
+++ subtitles-ukr.js
@@ -3,7 +3,7 @@
 
     var PLUGIN_ID = 'opensubtitles_ru2';
     var PLUGIN_TITLE = 'OpenSubtitles 2';
-    var DEFAULT_LANG = 'rus';
+    var DEFAULT_LANG = 'ukr';
 
     var ADDONS = [
         'https://opensubtitles-v3.strem.io'
